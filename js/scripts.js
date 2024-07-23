f3aecd870acdfba72b6a7c0016210aacb4e493e7
shubhamxthakur <stfushubham@gmail.com>
2024-07-23 22:51:46 +0530

    added html, css and js files

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,13 @@
- 
+document.querySelectorAll('.experience-header').forEach(header => {
+    header.addEventListener('click', () => {
+        const details = header.nextElementSibling;
+        const plusMinus = header.querySelector('span:last-child');
+        if (details.style.display === 'none' || details.style.display === '') {
+            details.style.display = 'block';
+            plusMinus.textContent = '-';
+        } else {
+            details.style.display = 'none';
+            plusMinus.textContent = '+';
+        }
+    });
+});
